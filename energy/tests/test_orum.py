fa51c787df07cfb2fcab2211a74c00848cd91083
kirill <demyanov.kirill@gmail.com>
2015-12-17 11:16:18 +0300

    перенос нескольких тестов в test_orum_setting

--- energy/tests/test_orum.py
+++ energy/tests/test_orum.py
@@ -11,24 +11,6 @@ class TestOrum(TestCase):
         self.types = {OrumType.objects.get(pk=x).formula: OrumType.objects.get(pk=x) for x in range(6, 9)}
         self.periods = {x: Period.objects.get(pk=x) for x in range(1, 11)}
 
-    def test_valid_orum_type(self):
-        """ Для орумов предусмотрено 8 типов """
-        self.assertEqual(OrumType.objects.all().count(), 8)
-
-    def test_hours_default_equal_1(self):
-        """ В орумах часы использования по умолчанию 1 """
-        orum = Orum.objects.create(type=self.types[1])
-        setting = OrumSetting.objects.create(orum=orum, power=3, ratio=1, installation_orum=self.periods[2])
-
-        self.assertEqual(setting.hours, 1)
-
-    def test_ratio_default_equal_1(self):
-        """ В орумах коэффициент по умолчанию 1 """
-        orum = Orum.objects.create(type=self.types[1])
-        setting = OrumSetting.objects.create(orum=orum, power=3, installation_orum=self.periods[2])
-
-        self.assertEqual(setting.ratio, 1)
-
     def test_none_period(self):
         """ Запрос по периоду, в котором не было/нет орума """
         orum = Orum.objects.create(type=self.types[1])
