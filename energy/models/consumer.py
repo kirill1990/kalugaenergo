b4f451631d890e8efef800d5740aaa291644078c
kirill <demyanov.kirill@gmail.com>
2015-12-25 08:08:57 +0300

    добавлены поля инн и кпп

--- energy/models/consumer.py
+++ energy/models/consumer.py
@@ -1,20 +1,32 @@
 # coding: utf8
-
-__author__ = 'Demyanov Kirill'
-
+from django.core.urlresolvers import reverse
 from django.db import models
 from consumer_type import ConsumerType
 from production_area import ProductionArea
 
+__author__ = 'Demyanov Kirill'
+
 
 class Consumer(models.Model):
-    ls = models.CharField('Лицевой счет', max_length=10)
-    name = models.CharField('Наименование потребителя', max_length=150)
-    type = models.ForeignKey(ConsumerType)
-    production_area = models.ForeignKey(ProductionArea, null=True)
+    ls = models.CharField(u'Лицевой счет', max_length=10)
+    name = models.CharField(u'Наименование потребителя', max_length=150)
+    inn = models.CharField(u'ИНН', max_length=20, blank=True)
+    kpp = models.CharField(u'КПП', max_length=20, blank=True)
+    type = models.ForeignKey(ConsumerType, verbose_name=u'Тип потербителя')
+    production_area = models.ForeignKey(ProductionArea,
+                                        verbose_name=u'Производственный участок',
+                                        null=True,
+                                        )
 
     def __str__(self):
-        return self.name
+        return '%s: %s' % (self.ls, self.name)
 
     def __unicode__(self):
         return u"%s" % self.__str__()
+    
+    def test(self):
+        self.point_set.count()
+        pass
+
+    def get_absolute_url(self):
+        return reverse('energy:consumer_detail', kwargs={'pk': self.id})
