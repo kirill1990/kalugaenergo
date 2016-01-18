4c7c7125dd5ddfb33006d450c06b8daa92efb02a
kirill <demyanov.kirill@gmail.com>
2016-01-18 13:11:50 +0300

    Редактированное синтекса

--- energy/models/orum_correction.py
+++ energy/models/orum_correction.py
@@ -7,10 +7,16 @@ __author__ = 'Demyanov Kirill'
 
 
 class OrumCorrection(models.Model):
-    orum = models.ForeignKey(Orum)
-    period = models.ForeignKey(Period, null=True)
-    kwh = models.DecimalField(u'Корректировка в kwh',
-                              decimal_places=5,
-                              max_digits=17,
-                              default=0
-                              )
+    orum = models.ForeignKey(
+        Orum
+    )
+    period = models.ForeignKey(
+        Period,
+        null=True
+    )
+    kwh = models.DecimalField(
+        u'Корректировка в kwh',
+        decimal_places=5,
+        max_digits=17,
+        default=0
+    )
