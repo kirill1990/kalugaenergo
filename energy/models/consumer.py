be6865fea4f39b8ce1f051d6601223fafbd1059c
kirill <demyanov.kirill@gmail.com>
2016-01-22 11:55:10 +0300

    refactor

--- energy/models/consumer.py
+++ energy/models/consumer.py
@@ -8,15 +8,39 @@ __author__ = 'Demyanov Kirill'
 
 
 class Consumer(models.Model):
-    ls = models.CharField(u'Лицевой счет', max_length=10)
-    name = models.CharField(u'Наименование потребителя', max_length=150)
-    inn = models.CharField(u'ИНН', max_length=20, blank=True)
-    kpp = models.CharField(u'КПП', max_length=20, blank=True)
-    type = models.ForeignKey(ConsumerType, verbose_name=u'Тип потербителя')
-    production_area = models.ForeignKey(ProductionArea,
-                                        verbose_name=u'Производственный участок',
-                                        null=True,
-                                        )
+    old_id = models.PositiveIntegerField(
+        u'Уникальный номер из т2',
+        unique=True,
+        blank=True,
+        null=True,
+    )
+    ls = models.CharField(
+        u'Лицевой счет',
+        max_length=10,
+    )
+    name = models.CharField(
+        u'Наименование потребителя',
+        max_length=150,
+    )
+    inn = models.CharField(
+        u'ИНН',
+        max_length=20,
+        blank=True,
+    )
+    kpp = models.CharField(
+        u'КПП',
+        max_length=20,
+        blank=True,
+    )
+    type = models.ForeignKey(
+        ConsumerType,
+        verbose_name=u'Тип потербителя',
+    )
+    production_area = models.ForeignKey(
+        ProductionArea,
+        verbose_name=u'Производственный участок',
+        null=True,
+    )
 
     def __str__(self):
         return '%s: %s' % (self.ls, self.name)
@@ -27,6 +51,9 @@ class Consumer(models.Model):
     def test(self):
         self.point_set.count()
         pass
+
+    def current_period(self):
+        return self.production_area.power_grid_region.current_period
 
     def get_absolute_url(self):
         return reverse('energy:consumer_detail', kwargs={'pk': self.id})
